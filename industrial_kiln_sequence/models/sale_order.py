d4ffa647b3dadb0714c258ecb31e5b87e56b502a
ndi-odoo <ndi@odoo.com>
2022-03-28 14:25:48 -0700

    display message in form when job number has not prefix and suffix

--- industrial_kiln_sequence/models/sale_order.py
+++ industrial_kiln_sequence/models/sale_order.py
@@ -3,9 +3,6 @@
 import logging
 
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
-
-_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -17,21 +14,12 @@ class SaleOrder(models.Model):
     has_job_number = fields.Boolean('job number set al least once for this record', default=False,store=True)
     plant_code = fields.Char(string='Plant Code', related='partner_id.plant_code')
     plant_code_sequence = fields.Char(string='Plant code sequence')
-    has_passed_set_method = fields.Boolean('has been saved', default=False, store=True)
-    _sql_constraints = [
-        ('date_order_conditional_required', "CHECK( (state IN ('sale', 'done') AND date_order IS NOT NULL) OR state NOT IN ('sale', 'done') )", "A confirmed sales order requires a confirmation date."),
-        # ('job_number_required', "CHECK( state IN ('sale', 'done') AND (job_number IS NULL OR job_number IS FALSE) )", "A confirmed sales order requires a job number."),
-    ]
-
-    # @api.depends('state')
+  
     def set_next_job_number_sequence(self):
         for order in self:
             if self.env['ir.config_parameter'].sudo().get_param("sale.job_number_activate"):
-                print('QQQQQQQue xuxa pasa aca')
-                # if order in self.filtered(lambda rec: rec.state in ['sale']):  
                 next_job_number=self.env['ir.sequence'].search([('code', '=', 'sale.order.job.number')]).number_next_actual     
-                if next_job_number:
-                    print('QQQQQQ set de sequence_job_number')  
+                if next_job_number: 
                     order.sequence_job_number=next_job_number
                     self.set_job_number()
                 else:   
@@ -66,14 +54,8 @@ class SaleOrder(models.Model):
 
     @api.onchange('prefix_job_number', 'suffix_job_number')
     def set_job_number(self):  
-        # add rec.state in ['sale'] condition if they disregard of a reserved unique number
-        print('SSSSSSSSSSSet job NUmber')
         for order in self:
             if order in self.filtered(lambda rec: rec.prefix_job_number and rec.prefix_job_number != '' and rec.sequence_job_number and rec.suffix_job_number and rec.prefix_job_number != ''):
-                print('OOOOOOOOOrder state',order.state)
-                print('OOOOOOOOOrder preix',order.prefix_job_number)
-                print('OOOOOOOOOrder suffix',order.suffix_job_number)
-                print('OOOOOOOOOrder invoice_status ',order.invoice_status)
                 order.job_number = order.prefix_job_number + order.sequence_job_number + order.suffix_job_number
                 # job number can change in a s.o but the sequence number will not increase
                 if not order.has_job_number:
@@ -83,10 +65,6 @@ class SaleOrder(models.Model):
                 order.job_number = False      
                 order.has_job_number = False 
 
-            # if order.sequence_job_number and not order.job_number:
-            # #     self.action_confirm() 
-            #     raise ValidationError(_('A job number must be set. Edit to select prefix and suffix'))
-
     @api.onchange('partner_id')
     def update_plant_code(self):
         for order in self.filtered(lambda rec: rec.plant_code):   
@@ -94,53 +72,7 @@ class SaleOrder(models.Model):
         for order in self.filtered(lambda rec: not rec.plant_code): 
             order.plant_code_sequence = False
 
-    # @api.onchange('sequence_job_number')
-    # def validate_sequence_job_number(self):
-    #     for order in self.filtered(lambda rec: not rec.prefix_job_number or rec.prefix_job_number == '' or not rec.suffix_job_number or rec.prefix_job_number == ''):
-    #         print('IIIIIIIIIIIIIIIIII and here')
-    #         raise ValidationError(_('A job number must be set. Edit to select prefix and suffix'))
-
-    # def action_edit_when_quotation_passes_to_sales_order(self):
-    #         view_form_id = self.env.ref('sale.view_order_form').id
-    #         action = self.env["ir.actions.actions"]._for_xml_id("sale.action_quotations")
-    #         action.update({
-    #             'views': [(view_form_id, 'form')],
-    #             'view_mode': 'form',
-    #             'context': {
-    #                 'form_view_initial_mode': 'edit', 
-    #             },
-    #         })
-    #         return action
-
-    # def action_redirect_to_quotations(self):
-    #     action = self.env["ir.actions.actions"]._for_xml_id("sale.action_quotations_with_onboarding")
-    #     action['domain'] = self._get_sale_utm_domain()
-    #     action['context'] = {'create': False}
-    #     return action   
-
     def action_confirm(self): 
-        print('AAAAAAAAAAAAAction confirm should go here')
-        for order in self:
-            print('BBBBBefore action confirm order name', order.name)
         self.set_next_job_number_sequence()
-        super(SaleOrder, self).action_confirm()  
-        for order in self:
-            print('AAAAAAAAAAAfter action confirm order name', order.name)
-        # for order in self.filtered(lambda rec: not rec.job_number):
-        #     print('EEEEEEEEEEEEElse order has passed set method')
-        #     raise ValidationError(_('A job number must be set. Edit to select prefix and suffix'))
-
-    @api.depends('state')
-    def validation_error(self):
-        for order in self.filtered(lambda rec: rec.state in [('sale')] and not rec.job_number ):
-            raise ValidationError(_('A job number must be set. Edit to select prefix and suffix'))
-
-    # @api.onchange('job_sequence_number')
-    # def _check_job_number_in_sales_order(self):
-    #     print("AAAAAAAAAAAAAqui esta la clave check number in sales order")
-    #     if self.env['ir.config_parameter'].sudo().get_param("sale.job_number_activate"):
-    #         for order in self:
-    #             if order.state in [('sale')] and (not order.prefix_job_number or not order.suffix_job_number): 
-    #                 raise ValidationError(_(
-    #                     "A job number must be set. Edit to select prefix and suffix"
-    #                 ))
+        super(SaleOrder, self).action_confirm()
+   
