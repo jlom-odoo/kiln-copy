726183718474d4319eee1c8cace1fefd60cb3ecf
dipa-odoo <dipa@odoo.com>
2022-08-31 13:34:15 -0700

    [IMP] backup before refactor

--- industrial_sale/__manifest__.py
+++ industrial_sale/__manifest__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': '',
+    'name': 'Industrial Kiln & Dryer group : Price calculated based on Margin',
     'version': '1.0',
     'category': 'Customization',
     'description': """
-        Task ID: 2886259
-        Quadgram: dipa
+Price calculated based on Margin
+===========================================================================
+    Task ID: 2886259
+    Quadgram: dipa
     """,
     'summary': '',
     'author': 'Odoo Inc',
