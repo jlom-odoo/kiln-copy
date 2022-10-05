ea3ab00b42b3197ad3ac477b382b650a0b93117b
gecm-odoo <gecm@odoo.com>
2022-10-05 12:34:44 -0500

    [ADD] Allow Group by without admin permits
    In this module, we eliminated the admin condition for grouping by in the mail.message model. This allows any user to group by in the reporting for Completed Activities view.

---
+++ industrial_kiln_mail/models/mail_message.py
@@ -0,0 +1,11 @@
+from odoo import fields, models, api
+
+
+class Message(models.Model):
+    _inherit = 'mail.message'
+
+    @api.model
+    def _read_group_raw(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        return super(Message, self.sudo())._read_group_raw(domain, fields, groupby, offset=offset,
+            limit=limit, orderby=orderby, lazy=lazy,
+        )
