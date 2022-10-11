20652181bc2233c62c5b84979b991342e7c78632
gecm-odoo <gecm@odoo.com>
2022-10-11 12:45:36 -0500

    [ADD] Allow Group by without admin permits
    In this module, we eliminated the admin condition for grouping by in the mail.message model. This allows any user to group by in the reporting for Completed Activities view.

---
+++ industrial_kiln_mail/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Industrialkiln : Group by feature in activity model',
+    'summary': 'Remove admin filter to group by feature in activity model',
+    'description': '''
+        This model is used to let any user group by feature in activity model.
+        The normal flow of odoo is to group by feature only for admin user.
+        Developer: [gecm]
+        Task ID: 2985387
+        Link to project: https://www.odoo.com/web#id=2985387&cids=17&model=project.task&view_type=form
+    ''',
+    'author': 'Odoo PS',
+    'website': 'https://www.odoo.com',
+    'category': 'Mail',
+    'version': '14.0.1.0.0',
+    'depends': [
+        'mail',
+    ],
+    'license': 'OPL-1',
+}
