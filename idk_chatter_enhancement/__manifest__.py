7c035433b3b7c91e8651acc85da66c16eeacb925
nimi-odoo <nimi@odoo.com>
2023-07-20 13:28:24 -0400

    [ADD] idk_chatter_enhancement: Added module

---
+++ idk_chatter_enhancement/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": "Industrial Kiln: Chatter Enhancement",
+    "summary": """
+        Specifying contacts on logged activities
+    """,
+    "description": """
+        Task ID: 3340051
+        Chatter enhancement to specify contacts on logged activities
+    """,
+    "license": "OPL-1",
+    "author": "Odoo, Inc.",
+    "website": "https://www.odoo.com",
+    "category": "Custom Development",
+    "version": "1.0.0",
+    "depends": ["mail"],
+    "data": [
+        "views/mail_activity_views.xml",
+        "data/mail_templates.xml"
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "idk_chatter_enhancement/static/src/models/activity.js",
+            "idk_chatter_enhancement/static/src/components/activity/activity.js"
+        ],
+        "web.assets_qweb": [
+            "idk_chatter_enhancement/static/src/components/activity/activity.xml"
+        ]
+    },
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
